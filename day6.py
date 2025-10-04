5910b4b918a40ef6405bcb7b3dcae3e7685aa837
Abhishek031-bit <ag332456@gmail.com>
2025-10-04 19:46:25 +0530

    Added day6 (pie chart)

---
+++ day6.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+categories = ["Freshman", "Sophomores", "Juniors", "Seniors"]
+values = np.array([300, 250, 275, 225])
+colors = ["red", "yellow", "blue", "green"]
+
+plt.pie(
+    values,
+    labels=categories,
+    autopct="%1.1f%%",
+    colors=colors,
+    explode=[0, 0, 0, 0.1],
+    shadow=True,
+    startangle=90,
+)
+
+plt.title("Bro Code College")
+
+plt.show()
